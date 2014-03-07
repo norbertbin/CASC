f4a64982d7c8250c9c7fe5b8523e3732ec153dd5
Norbert Binkiewicz <norbert@stat.wisc.edu>
2014-03-07 16:26:36 -0600

    Added R script with spectral clustering functions.

---
+++ code/spectralClusteringMethods.R
@@ -0,0 +1,156 @@
+###--------------------------------------------------------------------
+# functions to run various spectral clustering methods
+###--------------------------------------------------------------------
+
+
+# ---------------------------------------------------------------------
+# load any necessary libraries and scripts
+# ---------------------------------------------------------------------
+require(Matrix)
+source("irlbaMod.R")
+
+
+# ---------------------------------------------------------------------
+# MAIN FUNCTIONS
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# returns cluster memberships for CCA based clustering
+# ---------------------------------------------------------------------
+getCcaClusters = function(adjacencyMat, covariates, nBlocks,
+    method = "regLaplacian") {
+
+    randStarts = 10 #number of random starts for kmeans
+    
+    if(dim(covariates)[2] < nBlocks) {
+        stop("Number of covariates is less than the number of blocks.")
+    }
+
+    ccaSingVec = getCcaSvd(getGraphMatrix(adjacencyMat, method), covariates,
+        nBlocks)$singVec
+
+    return( kmeans(ccaSingVec, nstart = randStarts)$cluster )
+}
+
+# ---------------------------------------------------------------------
+# returns cluster memberships for CASC based clustering
+# ---------------------------------------------------------------------
+getCascClusters = function(adjacencyMat, covariates, hTuningParam,
+    nBlocks, method = "regLaplacian") {
+
+    randStarts = 10 #number of random starts for kmeans
+    
+    cascSingVec = getCascSvd(getGraphMatrix(adjacencyMat, method), covariates,
+        hTuningParam, nBlocks)$singVec
+
+    return( kmeans(cascSingVec, nstart = randStarts)$cluster )
+    
+}
+
+
+# ---------------------------------------------------------------------
+# returns cluster memberships for SC based graph clustering
+# ---------------------------------------------------------------------
+getGraphScClusters = function(adjacencyMat, nBlocks,
+    method = "regLaplacian") {
+
+    randStarts = 10 #number of random starts for kmeans
+
+    scSingVec = getScSvd(getGraphMatrix(adjacencyMat, method),
+        nBlocks)$singVec
+
+    return( kmeans(scSingVec, nstart = randStarts)$cluster )
+}
+
+# ---------------------------------------------------------------------
+# returns cluster memberships for SC based covariate clustering
+# ---------------------------------------------------------------------
+getCovScClusters = function(covariates, nBlocks) {
+
+    randStarts = 10 #number of random starts for kmeans
+
+    scSingVec = getScSvd(covariates, nBlocks)$singVec
+
+    return( kmeans(scSingVec, nstart = randStarts)$cluster )
+}
+
+
+# ---------------------------------------------------------------------
+# HELPER FUNCTIONS
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# returns left singular vectors and values for CCA based clustering
+# ---------------------------------------------------------------------
+getCcaSvd = function(graphMat, covariates, nBlocks) {
+
+    singDecomp = svd(graphMat %*% covariates, nu = nBlocks, nv = 0)
+
+    return( list(singVec = singDecomp$u[, 1:nBlocks],
+                 singVal = singDecomp$d) ) 
+}
+
+# ---------------------------------------------------------------------
+# returns left singular vectors and values for CASC based clustering
+# ---------------------------------------------------------------------
+getCascSvd = function(graphMat, covariates, hTuningParam, nBlocks) {
+
+    #insure irlba internal representation is large enough
+    if(nBlocks > 10) {
+        internalDim = 2 * nBlocks
+    }
+    else {
+        internalDim = 20
+    }
+
+    #define a custom matrix vector multiply function
+    matrixMulti = function(aList, aVector, transposeBool) {
+        return( as.vector(aList$graphMat %*% aVector +
+                          aList$hTuningParam * aList$covariates %*%
+                          (t(aList$covariates) %*% aVector)) )
+    } 
+
+    singDecomp = irlbaMod(list(graphMat = graphMat, covariates = covariates,
+        hTuningParam = hTuningParam), nu = nBlocks + 1, nv = 0,
+        m_b = internalDim, matmul = matrixMulti)
+
+    return( list(singVec = singDecomp$u[, 1:nBlocks],
+                 singVal = singDecomp$d) ) 
+}
+
+# ---------------------------------------------------------------------
+# returns left singular vectors and values for SC based clustering
+# ---------------------------------------------------------------------
+getScSvd = function(scMatrix, nBlocks) {
+
+    singDecomp = svd(scMatrix, nu = nBlocks, nv = 0)
+
+    return( list(singVec = singDecomp$u[, 1:nBlocks],
+                 singVal = singDecomp$d) ) 
+}
+
+
+# ---------------------------------------------------------------------
+# returns the graph matrix corresponding to the given method
+# ---------------------------------------------------------------------
+getGraphMatrix = function(adjacencyMat, method) {
+
+    if(method == "regLaplacian") {
+        rSums = rowSums(adjacencyMat)
+        tau = mean(rSums)
+        normMat = Diagonal(1/sqrt(rowSums(adjacencyMat + tau)))
+        graphMat = normMat %*% adjacencyMat %*% normMat
+    }
+    else if(method == "laplacian") {
+        normMat = Diagonal(1/sqrt(rowSums(adjacencyMat)))
+        graphMat = normMat %*% adjacencyMat %*% normMat
+    }
+    else if(method == "adjacency"){
+        graphMat = adjacencyMat
+    }
+    else {
+        stop("Method given not valid.")
+    }
+
+    return(graphMat)
+}
