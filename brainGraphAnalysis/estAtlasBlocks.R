a646ae11578c3d304755ae6852d15d617a674cb9
Norbert Binkiewicz <norbert@stat.wisc.edu>
2014-04-17 16:14:29 -0500

    Added estAtlasBlocks.R for estimating the block matrix using a brain atlas. Updated Makefile.

---
+++ brainGraphAnalysis/estAtlasBlocks.R
@@ -0,0 +1,107 @@
+# ---------------------------------------------------------------------
+# load packages and source files
+# ---------------------------------------------------------------------
+if (!require(oro.nifti)) {
+    install.packages('oro.nifti', dependencies = T)
+    require(oro.nifti)
+}
+
+if (!require(reshape2)) {
+    install.packages('reshape2', dependencies = T)
+    require(reshape2)
+}
+
+require(mclust)
+if (!require(mclust)) {
+    install.packages('mclust', dependencies = T)
+    require(mclust)
+}
+
+if (!require(R.matlab)) {
+    install.packages('R.matlab', dependencies = T)
+    require(R.matlab)
+}
+
+if (!require(Matrix)) {
+    install.packages('Matrix', dependencies = T)
+    require(Matrix)
+}
+
+source('../code/helperFunctions.R')
+
+# ---------------------------------------------------------------------
+# load atlas and other data
+# ---------------------------------------------------------------------
+comArgs = commandArgs(T)
+atlasFileName = comArgs[1]
+rawDataDir = comArgs[2]
+procDataDir = comArgs[3]
+filePre = comArgs[4]
+
+#define other directories
+outDir = "cache/"
+figDir = "figs/"
+
+#load graph
+graphInputFile = paste(rawDataDir, filePre, '_big_graph.mat', sep='')
+fiberGraph = readMat(graphInputFile)$fibergraph
+
+#load atlas
+atlasDat = readNIfTI(atlasFileName)
+
+#load clusters from cache file
+cascInputFile = paste(outDir, filePre, '_CASC', sep='')
+cascCluster = loadMatrix(cascInputFile, 1)
+scInputFile = paste(outDir, filePre, '_SC', sep='')
+scCluster = loadMatrix(scInputFile, 1)
+
+#load graph coordinates
+coordInputFile = paste(procDataDir, filePre, '_big_lcc.txt', sep='')
+coorData = read.table(lccCovInputFile)
+
+# only keep the largest connected component
+fiberGraph = fiberGraph[coorData$V1 + 1, coorData$V1 + 1]
+
+#create coordinate matrix
+coorMat = as.matrix(coorData)[,2:4]
+
+# ---------------------------------------------------------------------
+# convert data tensor to coordinate label matrix
+# match atlas to graph
+# ---------------------------------------------------------------------
+atlasMat = melt(atlasDat@.Data)
+atlasMat = atlasMat[atlasMat$value > 0,]
+
+# match atlas with data using location coordinates
+indexAM = atlasMat[,1] + atlasMat[,2]*10^3 + atlasMat[,3]*10^6
+indexCM = coorMat[,1] + coorMat[,2]*10^3 + coorMat[,3]*10^6
+matchIndex = match(indexCM, indexAM)
+sum(is.na(matchIndex))
+
+# atlas assignments for graph
+atlasCluster = atlasMat[matchIndex, 4]
+
+# compute ari for atlas vs casc and sc
+adjustedRandIndex(atlasCluster, cascCluster)
+adjustedRandIndex(atlasCluster, scCluster)
+
+# ---------------------------------------------------------------------
+# estimate block matrix using atlas and graph
+# ---------------------------------------------------------------------
+# exclude very small clusters
+nodeCounts = tabulate(atlasCluster)
+smallClusters = which(nodeCounts < 200)
+
+# set na's to new cluster
+idRemove = c( which(is.na(atlasCluster)), which(atlasCluster %in%
+    smallClusters))
+
+# estimate block matrix
+bMat = estBlockMat(fiberGraph[-idRemove, -idRemove], atlasCluster[-idRemove])
+
+# save block matrix in cache
+write.table(bMat, paste(outDir, filePre, '_estimatedB.txt')
+
+# plot estimate of block matrix
+plotB(bMat, paste(figDir, filePre, '_estimatedB.pdf'))
+
