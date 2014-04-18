105d74e4e906b0b872616cc0ed7b9d6fd6786e0b
Norbert Binkiewicz <norbert@stat.wisc.edu>
2014-04-17 19:37:40 -0500

    Added colSd funtion to helperFunctions.R.

--- code/helperFunctions.R
+++ code/helperFunctions.R
@@ -98,3 +98,14 @@ getVectorBins = function(vec, nBins) {
     nVec = length(vec)
     return(sort(vec)[round(nVec/nBins*(1:nBins))])
 }
+
+# compute column sd
+colSd = function(x, na.rm=TRUE) {
+  if (na.rm) {
+    n = colSums(!is.na(x))
+  } else {
+    n = nrow(x)
+  }
+  colVar =  colMeans(x*x, na.rm=na.rm) - (colMeans(x, na.rm=na.rm))^2
+  return(sqrt(colVar * n/(n-1)))
+}
