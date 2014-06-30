2d95607f5c0589b2ce2bd19426cd1caf435083bd
Norbert Binkiewicz <norbert@stat.wisc.edu>
2014-06-30 17:38:43 -0500

    Added hacks to compareBrainClusters to better compare pairs.

--- brainGraphAnalysis/compareBrainClusters.R
+++ brainGraphAnalysis/compareBrainClusters.R
@@ -15,6 +15,10 @@ if (!require(doMC)) {
     install.packages('doMC', dependencies = T)
     require(doMC)
 }
+if (!require(gplots)) {
+    install.packages('gplots', dependencies = T)
+    require(gplots)
+}
 
 source('../code/readWriteMatrix.R')
 
@@ -44,6 +48,7 @@ preVec = readLines(preListFile)
 # number of cores 
 registerDoMC(nCores)
 
+if(!file.exists(paste(outDir, "compareBrainClusters.bin", sep=""))) {
 # ---------------------------------------------------------------------
 # load clusters
 # ---------------------------------------------------------------------
@@ -103,14 +108,40 @@ for(i in 1:(nGraphs-1)) {
 # write ari matricies
 saveMatrixList(paste(outDir, "compareBrainClusters", sep=""),
                list(cascAri, ccaAri, scAri, scxAri))
+} else {
+	cascAri = loadMatrix(paste(outDir, "compareBrainClusters", sep=""), 1)
+	ccaAri = loadMatrix(paste(outDir, "compareBrainClusters", sep=""), 2)
+	scAri = loadMatrix(paste(outDir, "compareBrainClusters", sep=""), 3)
+	scxAri = loadMatrix(paste(outDir, "compareBrainClusters", sep=""), 4)
+}
 
-# do hierarchical clustering using ari
-pdf(paste(figDir, "cascAriHier.pdf"), width = 10, heigh = 10)
-plot(hclust(as.dist(t(cascAri))), xlab = "", main = "Cluster Dendrogram Using
-ARI for CASC")
+ordLabels = c(1, 25, 2, 37, 3, 22, 4, 11, 5, 31, 6, 20, 7, 34, 8, 29, 9, 42, 10, 21, 12, 19, 13, 24, 14, 17, 15, 26, 16, 35, 18, 38, 23, 27, 28, 40, 30, 33, 32, 36, 39, 41)
+
+# draw ari matrix
+pdf(paste(figDir, "compareScAri.pdf", sep=""), width = 10, height = 10)
+heatmap.2(scAri, Rowv = NULL, Colv = NULL, dendrogram = "none", key = F, trace = "none", notecol="dodgerblue2", col = colorRampPalette(c("white", "black"))(256), lwid = c(.09, .99), lhei = c(.01, .99), margins = c(5, 5), cellnote = round(scAri, 2), labRow = ordLabels, labCol = ordLabels)
 dev.off()
 
-pdf(paste(figDir, "rscAriHier.pdf"), width = 10, heigh = 10)
-plot(hclust(as.dist(t(scAri))), xlab = "", main = "Cluster Dendrogram Using
-ARI for RSC")
+pdf(paste(figDir, "compareCcaAri.pdf", sep=""), width = 10, height = 10)
+heatmap.2(ccaAri, Rowv = NULL, Colv = NULL, dendrogram = "none", key = F, trace = "none", notecol="dodgerblue2", col = colorRampPalette(c("white", "black"))(256), lwid = c(.09, .99), lhei = c(.01, .99), margins = c(5, 5), cellnote = round(ccaAri, 2), labRow = ordLabels, labCol = ordLabels)
+dev.off()
+
+pdf(paste(figDir, "compareCascAri.pdf", sep=""), width = 10, height = 10)
+heatmap.2(cascAri, Rowv = NULL, Colv = NULL, dendrogram = "none", key = F, trace = "none", notecol="dodgerblue2", col = colorRampPalette(c("white", "black"))(256), lwid = c(.09, .99), lhei = c(.01, .99), margins = c(5, 5), cellnote = round(cascAri, 2), labRow = ordLabels, labCol = ordLabels)
+dev.off()
+
+# do hierarchical clustering using ari
+pdf(paste(figDir, "cascAriHier.pdf", sep=""), width = 10, heigh = 10)
+plot(hclust(as.dist(t(1-cascAri))), xlab = "", main = "Cluster Dendrogram Using
+ARI for CASC", labels = ordLabels)
+dev.off()
+
+pdf(paste(figDir, "rscAriHier.pdf", sep=""), width = 10, heigh = 10)
+plot(hclust(as.dist(t(1-scAri))), xlab = "", main = "Cluster Dendrogram Using
+ARI for RSC", labels = ordLabels)
+dev.off()
+
+pdf(paste(figDir, "ccaAriHier.pdf", sep=""), width = 10, heigh = 10)
+plot(hclust(as.dist(t(1-ccaAri))), xlab = "", main = "Cluster Dendrogram Using
+ARI for CCA", labels = ordLabels)
 dev.off()
