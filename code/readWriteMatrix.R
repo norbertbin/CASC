83ae6dca42dd4c9c1683a51af979285e573a8e50
Norbert Binkiewicz <norbert@stat.wisc.edu>
2014-07-18 13:27:24 -0500

    Added sparse matrix read/write functions.

--- code/readWriteMatrix.R
+++ code/readWriteMatrix.R
@@ -42,3 +42,26 @@ loadMatrix <- function(baseName = "data", index) {
     type <- readBin(con, 'character', 1)
     structure(readBin(con, type, prod(d)), dim=d)
 }
+
+# ---------------------------------------------------------------------
+# Functions to read/write sparse matrix into a binary file
+# ---------------------------------------------------------------------
+saveSparseMatrix = function(baseName, sMat) {
+    wSMat = as.matrix(summary(sMat))
+    wSMat = rbind(wSMat, c(sMat@Dim, 0))
+
+    saveMatrixList(baseName, list(wSMat))
+}
+
+loadSparseMatrix = function(baseName) {
+    rSMat = loadMatrix(baseName, 1)
+
+    nrows = dim(rSMat)[1]
+    dims = rSMat[nrows, 1:2]
+    rSMat = rSMat[-nrows,]
+
+    return(sparseMatrix(i = rSMat[,1],
+                        j = rSMat[,2],
+                        x = rSMat[,3],
+                        dims = dims))
+}
